c922da8b75ed99e25d2614b2173c3631967cb143
Matt Juhl <matt@juhl.me>
2024-12-30 08:27:43 -0700

    added completion setup to nvim config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -256,6 +256,83 @@ require("lazy").setup({
 				})
 			end,
 		},
+
+		-- ### completion ### --
+		{
+			"hrsh7th/cmp-nvim-lsp",
+		},
+		{
+			"L3MON4D3/LuaSnip",
+			dependencies = {
+				"hrsh7th/cmp-nvim-lsp-signature-help",
+				"saadparwaiz1/cmp_luasnip",
+				"rafamadriz/friendly-snippets",
+			},
+			version = "v2.*",
+
+			-- see: https://github.com/L3MON4D3/LuaSnip/blob/master/DOC.md#transformations
+			-- build = see: "make install_jsregexp",
+		},
+		{
+			"hrsh7th/nvim-cmp",
+			config = function()
+				local luasnip = require("luasnip")
+				local cmp = require("cmp")
+				require("luasnip.loaders.from_vscode").lazy_load()
+
+				cmp.setup({
+					snippet = {
+						expand = function(args)
+							require("luasnip").lsp_expand(args.body)
+						end,
+					},
+					window = {
+						completion = cmp.config.window.bordered(),
+						documentation = cmp.config.window.bordered(),
+					},
+					mapping = {
+						["<Tab>"] = cmp.mapping(function(fallback)
+							if cmp.visible() then
+								cmp.select_next_item()
+							elseif luasnip.locally_jumpable(1) then
+								luasnip.jump(1)
+							else
+								fallback()
+							end
+						end, { "i", "s" }),
+
+						["<S-Tab>"] = cmp.mapping(function(fallback)
+							if cmp.visible() then
+								cmp.select_prev_item()
+							elseif luasnip.locally_jumpable(-1) then
+								luasnip.jump(-1)
+							else
+								fallback()
+							end
+						end, { "i", "s" }),
+						["<CR>"] = cmp.mapping({
+							i = function(fallback)
+								if cmp.visible() and cmp.get_active_entry() then
+									cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+								else
+									fallback()
+								end
+							end,
+							s = cmp.mapping.confirm({ select = true }),
+							c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+						}),
+					}, -- /mapping
+					sources = cmp.config.sources({
+						{ name = "nvim_lsp" },
+						{ name = "luasnip" },
+						{ name = "nvim_lsp_signature_help" },
+					}, {
+						{ name = "buffer" },
+					}), -- /sources
+				})
+			end,
+		},
+		-- end of completion stuff
 	},
 	-- Configure any other settings here. See the documentation for more details.
 	-- colorscheme that will be used when installing plugins.
