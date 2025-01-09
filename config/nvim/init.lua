2e4f941d8d9fe969aca2b9b58564174f437eaf3a
Matt Juhl <matt@juhl.me>
2025-01-08 23:45:47 -0700

    going up

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -62,6 +62,14 @@ require("lazy").setup({
 			"folke/which-key.nvim",
 		},
 		{
+			"nullromo/go-up.nvim",
+			opts = {},
+			config = function(_, opts)
+				local goUp = require("go-up")
+				goUp.setup(opts)
+			end
+		},
+		{
 			"christoomey/vim-tmux-navigator",
 			cmd = {
 				"TmuxNavigateLeft",
