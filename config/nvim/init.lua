5592492384da0944910f5e71b538d86d69db8aa8
Matt Juhl <matt@juhl.me>
2025-01-08 20:30:31 -0700

    update layout

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -117,8 +117,11 @@ require("lazy").setup({
 				local telescope = require("telescope")
 				telescope.setup({
 					defaults = {
-						-- layout_strategy = "cursor",
-						layout_config = {},
+						layout_strategy = "horizontal",
+						layout_config = {
+							prompt_position = "top",
+						},
+						sorting_strategy = "ascending",
 					},
 					extensions = {
 						["ui-select"] = {
