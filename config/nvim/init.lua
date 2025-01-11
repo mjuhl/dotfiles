77d2a8b6c0f6526a3782abe0b5153c59ad98ce4e
Matt Juhl <matt.juhl@simpleviewinc.com>
2025-01-10 23:19:53 -0700

    bbq

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -321,6 +321,18 @@ require("lazy").setup({
 				words = { enabled = true },
 			},
 		},
+		{
+			"utilyre/barbecue.nvim",
+			name = "barbecue",
+			version = "*",
+			dependencies = {
+				"SmiteshP/nvim-navic",
+				"nvim-tree/nvim-web-devicons", -- optional dependency
+			},
+			opts = {
+				-- configurations go here
+			},
+		},
 
 		-- ### LSP Related ### --
 		{
