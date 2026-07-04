af513e2878a2f80a33a113cd46f6551ecf6a60ee
Matt Juhl <mjuhl@Matts-Mac-Studio.local>
2026-07-03 17:06:20 -0700

    update deprecated call

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -710,7 +710,7 @@ require("lazy").setup({
 -- highlight yanked text
 vim.api.nvim_create_autocmd("TextYankPost", {
 	callback = function()
-		vim.highlight.on_yank()
+		vim.hl.on_yank()
 	end,
 })
 
